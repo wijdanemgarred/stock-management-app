27995465a1ebc52753d3ef1986bd3e794867c3f1
wijdane121 <wijdanemg17@gmail.com>
2024-11-12 11:24:57 +0100

    Initial commit

---
+++ src/test/java/uir/ac/ma/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package uir.ac.ma.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
